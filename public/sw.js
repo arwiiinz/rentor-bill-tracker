530c4004ba28a3344d46f64e40d97775eaa9f67b
arwiiinz <76207252+arwiiinz@users.noreply.github.com>
2026-05-08 09:16:12 +0800

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', event => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(clients.claim()));
+
+self.addEventListener('push', event => {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: '/icons/icon-192.png',
+        badge: '/icons/icon-72.png',
+        vibrate: [200, 100, 200],
+        data: { url: data.url || '/' }
+    };
+    event.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+});
